e94b6820f86301f172a824dc1fa1a08e7ecc2014
Brecht Latruwe <brecht.latruwe@gmail.com>
2023-12-06 21:58:58 +0100

    refactoring day6

--- day6/part2.py
+++ day6/part2.py
@@ -1,15 +1,8 @@
 f=open("day6/input.txt")
-
-
 for line in f:
     if "Time" in line:
         time=int(line.strip().split(":")[1].replace(' ',''))
     elif "Distance" in line:
         distance=int(line.strip().split(":")[1].replace(' ',''))
 
-
-
-count=0
-for btnpushed in range(time):
-    if btnpushed*(time-btnpushed)>distance: count+=1
-print(count)
+print(sum(1 for btnpushed in range(time) if btnpushed*(time-btnpushed)>distance))
