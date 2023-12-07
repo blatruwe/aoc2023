ad12de8db3231dd2c9fbb4fe00d5d52b6850d031
Brecht Latruwe <brecht.latruwe@gmail.com>
2023-12-07 21:24:21 +0100

    day7

---
+++ day7/part1.py
@@ -0,0 +1,58 @@
+f=open("day7/input.txt")
+
+
+
+def gethandtype(cards):
+    """
+    1=high card
+    2=one pair
+    3=two pair
+    4=three of a kind
+    5=full house
+    6=four of a kind
+    7=full house
+    """
+    count_occurence=[]
+    for i in range(len(cards)):
+        #check same cards
+        count_occurence.append(cards.count(cards[i]))
+    if 5 in count_occurence: return 7
+    elif 4 in count_occurence: return 6
+    elif 3 in count_occurence:
+        if 2 in count_occurence: return 5
+        return 4
+    elif 2 in count_occurence:
+        if count_occurence.count(2)==4:
+            return 3
+        return 2
+    return 1
+
+
+
+hands=[]
+for line in f:
+    cards=line.strip().split()[0]
+    handType=gethandtype(cards)
+    card_value=[]
+    for card in cards:
+        if card=='A':
+            card_value.append(14)
+        elif card=='K':
+            card_value.append(13)
+        elif card=='Q':
+            card_value.append(12)
+        elif card=='J':
+            card_value.append(11)
+        elif card=='T':
+            card_value.append(10)
+        else:
+            card_value.append(int(card))
+    hands.append((handType,card_value,int(line.strip().split()[1])))
+
+
+rank=0
+total=0
+for hand in sorted(hands):
+    rank+=1
+    total+=rank*hand[2]
+print(total)
